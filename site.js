c0885253ffe13abe93324146db1b98550fac80cb
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-05-26 16:39:52 -0400

    Rossmoor 1.0  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "The Shops at Rossmoor Seal Beach",
+                    "default_image" : "http://assets.codecloudapp.com/sites/57475db66e6f644797520000/image/jpeg/1463757854000/dgvr_500x500_default.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "instagram" : "196558634"
+                }
