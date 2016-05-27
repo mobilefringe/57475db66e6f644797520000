bfc550f9b457bc3416e6fac2234c4ef5f96dad78
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-05-27 11:43:10 -0400

    Rossmoor 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "The Shops at Rossmoor",
                     "default_image" : "http://assets.codecloudapp.com/sites/57475db66e6f644797520000/image/jpeg/undefined/rsb_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "instagram" : "196558634"
+                    "instagram" : "1590821057"
                 }
