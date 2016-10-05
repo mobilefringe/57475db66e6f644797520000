def775b34304405479bc236e6f10f86164606bf3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-05 10:21:52 -0400

    Rossmoor 1.1  /site.js

---
+++ site.js
@@ -0,0 +1,8 @@
+var site_json = {
+                    "name" : "The Shops at Rossmoor",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f50a1b6e6f6426d2010000/image/jpeg/undefined/rsb_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "instagram" : "1590821057"
+                }
+                
+                
